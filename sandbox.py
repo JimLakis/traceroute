64c18f8fc6d8df5492cff3e55db4a73e3baebb84
Jim Lakis <ejames.b1@gmail.com>
2024-07-10 16:56:43 -0700

    adding thread module and first experiment adding Thread() to project.

--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,7 @@
 ## Author: Jim Lakis
 
+from threading import Thread, Lock
+
 ## scapy==2.5.0
 from scapy.sendrecv import sr1
 from scapy.layers.inet import IP, TCP
@@ -8,6 +10,8 @@ import signal
 import sys
 import time
 
+
+lock = Lock()
 
 def make_ip_packet(dst: str = "www.Google.com", ttl: tuple = (1), *args, **kwargs):
     try:
@@ -28,56 +32,42 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 def perform_single_ttl_traceroute(ip_packet, tcp_packet, timeout: int = 5, *args, **kwargs):
-    start_time = time.time()
-    response = None
-    while time.time() - start_time < timeout:
-        remaining_time = timeout - (time.time() - start_time)
-        if remaining_time <= 0:
-            break
-        try:
-            response = sr1(ip_packet / tcp_packet, timeout=min(1, remaining_time), verbose=False)
-        except Exception as e:
-            print(f"An unexpected error occurred when running the sr1() function: {e}")
-
-        if response is not None:
-            break
-    return response
-
-
-def main(responses):
-    '''
-    '''
-    print(f"Starting traceroute... enter 'ctrl+c' to force quit\n")
-
-    signal.signal(signal.SIGINT, signal_handler)
-
-    try:
-        tcp_packet = make_tcp_packet()
-        i = 1
-        while i <= 64:
-            ip_packet = make_ip_packet(ttl=i)
-            reply_packet = perform_single_ttl_traceroute(ip_packet, tcp_packet, timeout = 5)
-            k = f"{str(i)}"
-            print(f"ttl {k} complete")
-            if isinstance(reply_packet, IP):
-                #v = f"{str(reply_packet[IP].src)}"
-                v = reply_packet
-                if v in responses.values():
-                    break
-                else:
-                    responses.update({k: v})
-                    print(f"answering IP address {v}")
-            else:
-                print(f"Response is not an IP packet")
+    with lock:
+        start_time = time.time()
+        response = None
+        while time.time() - start_time < timeout:
+            remaining_time = timeout - (time.time() - start_time)
+            if remaining_time <= 0:
                 break
-            i+=1
-    except Exception as e:
-        print(f"An unexpected error occurred within the main() function: {e}")
-    finally:
-        return responses
+            try:
+                response = sr1(ip_packet / tcp_packet, timeout=min(1, remaining_time), verbose=False)
+            except Exception as e:
+                print(f"An unexpected error occurred when running the sr1() function: {e}")
 
+            if response is not None:
+                break
+        return response
+
+
+def main():
+    '''Theads on YouTube
+    ## https://www.youtube.com/watch?v=m70_u0DPK5k
+    '''
+    threads = []
+
+    tcp_packet = make_tcp_packet()
+    for i in range(64):
+        ip_packet = make_ip_packet(ttl=i)
+        single_thread = Thread(target=perform_single_ttl_traceroute, args=(ip_packet, tcp_packet))
+        threads.append(single_thread)
+        single_thread.start()
+
+    for t in threads:
+        t.join()
+
+    print(threads)
 
 if __name__ == "__main__":
-    responses = {}
-    main(responses)
-    print(responses)
+    print(f"Starting traceroute... enter 'ctrl+c' to force quit\n")
+    signal.signal(signal.SIGINT, signal_handler)
+    main()
